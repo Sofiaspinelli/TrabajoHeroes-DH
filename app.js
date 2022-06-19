685afa525b22584285045848b39837f469176421
Sofia <sofiaspinelli17@gmail.com>
2022-06-19 18:47:18 -0300

    correcciones de personajes

--- app.js
+++ app.js
@@ -13,5 +13,6 @@ app.get ('/berners-lee', (req,res)=> res.sendFile(path.resolve (__dirname, 'view
 app.get ('/clarke', (req,res)=> res.sendFile(path.resolve (__dirname, 'views', 'clarke.html')))
 app.get ('/babbage', (req,res)=> res.sendFile(path.resolve (__dirname, 'views', 'babbage.html')))
 app.get ('/hamilton', (req,res)=> res.sendFile(path.resolve (__dirname, 'views', 'hamilton.html')))
+app.get ('/hopper', (req,res)=> res.sendFile(path.resolve (__dirname, 'views', 'hopper.html')))
 
-app.listen (port, () => console.log (`Se levanto con exito el servidor en http://localhost:${port}`))
+app.listen (port, () => console.log (`Se levanto con exito el servidor http://localhost:${port}`))
